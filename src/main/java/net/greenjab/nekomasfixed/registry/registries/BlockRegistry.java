f2d04417f3cabd7c7ed5aec9c10444a1dd64ad2c
Strikey5852 <strikinglegend5852@gmail.com>
2026-09-08 14:18:01 +0530

    applied the same breaking speed and block sounds as vanilla cake

--- src/main/java/net/greenjab/nekomasfixed/registry/registries/BlockRegistry.java
+++ src/main/java/net/greenjab/nekomasfixed/registry/registries/BlockRegistry.java
@@ -78,14 +78,14 @@ public class BlockRegistry {
                     .pushReaction(PushReaction.DESTROY)
     );
 
-    public static final Block SWEETBERRY_CAKE = register("sweetberry_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
-    public static final Block PAN_CAKE = register("pan_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
-    public static final Block GLOWBERRY_CAKE = register("glowberry_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
-    public static final Block APPLE_CAKE = register("apple_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
-    public static final Block VANILLA_CAKE = register("vanilla_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
-    public static final Block COOKIE_CAKE = register("cookie_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
-    public static final Block CHOCOLATE_CAKE = register("chocolate_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
-    public static final Block BEETROOT_CAKE = register("beetroot_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
+    public static final Block SWEETBERRY_CAKE = register("sweetberry_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().strength(0.5F).sound(SoundType.WOOL).lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
+    public static final Block PAN_CAKE = register("pan_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().strength(0.5F).sound(SoundType.WOOL).lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
+    public static final Block GLOWBERRY_CAKE = register("glowberry_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().strength(0.5F).sound(SoundType.WOOL).lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
+    public static final Block APPLE_CAKE = register("apple_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().strength(0.5F).sound(SoundType.WOOL).lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
+    public static final Block VANILLA_CAKE = register("vanilla_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().strength(0.5F).sound(SoundType.WOOL).lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
+    public static final Block COOKIE_CAKE = register("cookie_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().strength(0.5F).sound(SoundType.WOOL).lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
+    public static final Block CHOCOLATE_CAKE = register("chocolate_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().strength(0.5F).sound(SoundType.WOOL).lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
+    public static final Block BEETROOT_CAKE = register("beetroot_cake", StackedCakeBlock::new, BlockBehaviour.Properties.of().strength(0.5F).sound(SoundType.WOOL).lightLevel(state -> state.getValue(StackedCakeBlock.LIT)?3:0));
 
     static BlockSetType BAOBAB_BLOCKSETTYPE = BlockSetType.register(new BlockSetType("baobab"));
     static WoodType BAOBAB_WOODTYPE = WoodType.register(new WoodType("baobab", BAOBAB_BLOCKSETTYPE));
